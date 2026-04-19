82d113fa98ae589dba8f164c3fdd455ffc739c2e
Gaston <gastonyirula86@gmail.com>
2026-04-19 02:57:11 -0300

    se acomodo el homePage y se le dio un nuevo estilo

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getAllGames } from '../../services/api';
 import styles from './homePage.module.css';
 import Navbar from '../../components/navBar/navBar';
 import logo from '../../assets/images/logo.png';
 import homeImage from '../../assets/images/PcGamer.jpg';
 
-function HomePage(){
+function HomePage( onNavigate ) {
+    const navigate = useNavigate();
     const [games, setGames] = useState([])
 
     useEffect(() => {
         async function loadGames(){
-            const data = await getAllGames()
-            setGames(data)
+            try{
+                const data = await getAllGames()
+            
+                const randomGames = data
+                    .sort(() => Math.random() - 0.5)
+                    .slice(0, 10)
+
+                setGames(randomGames)
+            } catch (error) {
+                console.error('Error al cargar juegos', error)
+            }
         }
 
         loadGames()
@@ -19,25 +30,44 @@ function HomePage(){
 
     return(
         <div className={styles.homePageWrapper}>
-            <Navbar />
             {/* Header con el logo*/}
             <header className={styles.header}>
                 <img
-                className={styles.logos} 
+                className={styles.logo} 
                 src={logo} alt="logo" />
             </header>
 
             {/* Contenido principal */}
             <div className={styles.main}>
                 <div className={styles.heroSection}>
-                    <h1 className={styles.tittle}>GamX</h1>
-                    <p className={styles.description}>
-                        Bienvenido a GamX, la mejor plataforma para los amantes de los videojuegos. Explora e investiga sobre los mejores videojuegos free to play.
-                    </p>
+                    <img className={styles.heroImage} src={homeImage} alt="Imagen de fondo"/>
+                    <div className={styles.Text}>
+                        <h1 className={styles.title}>GamX</h1>
+                        <p className={styles.description}>
+                            Bienvenido a GamX, la mejor plataforma para los amantes de los videojuegos. Explora e investiga sobre los mejores videojuegos free to play.
+                        </p>
+                    </div>
                 </div>
 
-
+                <section className={styles.section}>
+                    <div className={styles.sectionHeader}>
+                        <h2>Destacados</h2>
+                    </div>
+                    <div className={styles.resultList}>
+                        {games.map(game => (
+                            <div key={game.id} className={styles.gameCard} onClick={() => navigate(`/game/${game.id}`)}>
+                                <img className={styles.cardThumb} src={game.thumbnail} alt={game.title} loading="lazy" />
+                                <div className={styles.cardInfo}>
+                                    <p className={styles.cardTitle}>{game.title}</p>
+                                    <p className={styles.cardGenre}>{game.genre}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section> 
             </div>
+
+            <Navbar currentPage="home" onNavigate={onNavigate} />
         </div>
     )
 }
