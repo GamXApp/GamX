a82780e6eed1f33603555063d4884d2514724e0b
Cristian <cristianseg0122@gmail.com>
2026-04-24 00:48:34 -0300

    guardar cambios

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,7 @@ import WishlistPage from './pages/wishList/wishListPage';
 import HistoryPage  from './pages/history/historyPage';
  
 function App() {
-<<<<<<< HEAD
-    // Página activa: 'home' | 'search' | 'wishlist' | 'history' | 'contact'
     const [currentPage, setCurrentPage] = useState('home');
-
-    function renderPage() {
-        switch (currentPage) {
-            case 'home':     return <HomePage     onNavigate={setCurrentPage} />;
-            case 'search':   return <SearchPage    onNavigate={setCurrentPage} />;
-            // case 'wishlist': return <WishlistPage  onNavigate={setCurrentPage} />;
-            // case 'history':  return <HistoryPage   onNavigate={setCurrentPage} />;
-            case 'contact':  return <ContactPage   onNavigate={setCurrentPage} />;
-            default:         return <SearchPage    onNavigate={setCurrentPage} />;
-        }
-    }
-
-    return renderPage();
-=======
     return (
         <BrowserRouter>
             <Routes>
@@ -39,7 +23,6 @@ function App() {
             </Routes>
         </BrowserRouter>
     );
->>>>>>> 49cee4632571c80ff724fa6c0b1d83f53439312a
 }
 
 export default App;
